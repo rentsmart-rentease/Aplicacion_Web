d4c93f7aed4dd0d5a35f1f66c0fbd6b4749ca0fe
u202210494 <u202210494@upc.edu.pe>
2024-06-07 10:14:28 -0500

    chore: Configuración del módulo `multimedia`.

--- src/app/multimedia/multimedia.module.ts
+++ src/app/multimedia/multimedia.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UploadComponent } from './upload/upload.component';
-import { UnsupportedFormatErrorComponent } from './unsupported-format-error/unsupported-format-error.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { UploadComponent } from "./upload/upload.component";
+import { UnsupportedFormatErrorComponent } from "./unsupported-format-error/unsupported-format-error.component";
 
 @NgModule({
-  declarations: [
-    UploadComponent,
-    UnsupportedFormatErrorComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UploadComponent, UnsupportedFormatErrorComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [UploadComponent],
 })
-export class MultimediaModule { }
+export class MultimediaModule {}
