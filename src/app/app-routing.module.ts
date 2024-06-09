6be9deafb618852415e69561ddbdbe39d65f66ab
Edu <eduardo14.05rivas@gmail.com>
2024-06-09 13:20:43 -0500

    feat: Implementación de la sección de publicaciones destacadas, y un formulario para agregar propiedades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./resultados/resultados.module').then(m => m.ResultadosModule) },
+  { path: 'publinmueble', loadChildren: () => import('./publ-inmueble/publ-inmueble.module').then(m => m.PublInmuebleModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
