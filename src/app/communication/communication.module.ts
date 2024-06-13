5ec1e7fdc8b40332170eca61b781e5081a948b5a
u202210494 <u202210494@upc.edu.pe>
2024-06-13 09:48:00 -0500

    chore: Configuración del módulo .

--- src/app/communication/communication.module.ts
+++ src/app/communication/communication.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MessageComponent } from './message/message.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MessageComponent } from "./message/message.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    MessageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MessageComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [MessageComponent],
 })
-export class CommunicationModule { }
+export class CommunicationModule {}
