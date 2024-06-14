57009afb04bbba1ae1ce6137209a00d1b7876f64
TarikMorales <145626466+TarikMorales@users.noreply.github.com>
2024-06-13 22:13:57 -0500

    feat: implementación de filtros en la búsqueda de propiedad y ruta a otro componente a partir de botones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BarraFiltroComponent } from './barra-filtro/barra-filtro.component';
+import { BuscaVisualizacionComponent } from './busca-visualizacion/busca-visualizacion.component';
+
+const appRoutes: Routes = [
+  { path: '', component: BarraFiltroComponent }, // Ruta predeterminada
+  // Otras rutas según sea necesario
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BarraFiltroComponent,
+    BuscaVisualizacionComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
