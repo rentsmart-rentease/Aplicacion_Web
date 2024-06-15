c3eb7e7ad2a64db2c1450aa0cd6abe0fe0e47cda
u202210494 <u202210494@upc.edu.pe>
2024-06-15 11:15:29 -0500

    chore: Agregado el componente `schedule`.

---
+++ src/app/appointments/appointments.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+
+
+@NgModule({
+  declarations: [
+    ScheduleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppointmentsModule { }
