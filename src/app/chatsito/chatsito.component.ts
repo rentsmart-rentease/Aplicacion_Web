57a0a3dc768c980ff8259bcb2f757bc70757fecd
leo-dev11 <u202213866@upc.edu.pe>
2024-06-17 17:45:32 -0500

    feat: Implementación del diseño del chat, estructurado y con estilos. Falta añadir el script para enviar el mensaje y que se muestre dentro del chat.

---
+++ src/app/chatsito/chatsito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatsito',
+  templateUrl: './chatsito.component.html',
+  styleUrl: './chatsito.component.css'
+})
+export class ChatsitoComponent {
+
+}
