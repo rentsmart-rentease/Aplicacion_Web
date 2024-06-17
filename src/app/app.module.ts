db4c642ecceea7dcc218fea257ec18d1c6be5bc2
leo-dev11 <u202213866@upc.edu.pe>
2024-06-17 02:07:30 -0500

    feat: Implementación de la página de información de un inmueble, se implementó la estructura html y los estilos. Necesita mejoras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InfoPropComponent } from './info-prop/info-prop.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InfoPropComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    NgbModule,
+    MatButtonModule
   ],
   providers: [
     provideAnimationsAsync()
