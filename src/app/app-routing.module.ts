9a5f7b50b4e5493833dbd027b13a37c43617ba1e
TarikMorales <145626466+TarikMorales@users.noreply.github.com>
2024-06-16 23:16:02 -0500

    feat: implemente sección para publicar una reseña a una propiedad ya alquilada, con la posibilidad de poder dejar tu comentario sobre la reseña y un boton de enviar la reseña

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroResenaComponent } from './registro-resena/registro-resena.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registro-resena', component: RegistroResenaComponent },
+  { path: '', redirectTo: '/registro-resena', pathMatch: 'full' }, // Redireccionar a la ruta de la reseña por defecto
+  { path: '**', redirectTo: '/registro-resena', pathMatch: 'full' } // Manejo de rutas no existentes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RegistroResenaComponent } from './registro-resena/registro-resena.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroResenaComponent
   ],
   imports: [
     BrowserModule,
