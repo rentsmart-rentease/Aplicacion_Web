8298c1525d7b175ba67a2e30707fe6b4df672aab
TarikMorales <145626466+TarikMorales@users.noreply.github.com>
2024-06-16 19:48:47 -0500

    feat: implemente las reseñas sobre una propiedad, mostrando las reseñas de 2 usuarios en este caso, con contenedor para busqueda de reseñas con filtros, y una calificacion promedio al inicio de la seccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisualizacionResenasComponent } from './visualizacion-resenas/visualizacion-resenas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'resenas', component: VisualizacionResenasComponent },
+  { path: '', redirectTo: '/resenas', pathMatch: 'full' },
+  // otras rutas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { VisualizacionResenasComponent } from './visualizacion-resenas/visualizacion-resenas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisualizacionResenasComponent
   ],
   imports: [
     BrowserModule,
