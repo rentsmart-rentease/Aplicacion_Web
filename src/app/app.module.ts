71a94551aceae9f18b8b5473cb2b075fe49906b2
unknown <u20221c621@upc.edu.pe>
2024-06-18 00:50:32 -0500

    Segundo Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ListingComponent } from './listing/listing.component';
+import { AddListingComponent } from './add-listing/add-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingComponent,
+    AddListingComponent
   ],
   imports: [
     BrowserModule,
