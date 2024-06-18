162e2ac1916cb2c162f47e7ea3fe7ee650a22534
leo-dev11 <u202213866@upc.edu.pe>
2024-06-17 19:21:35 -0500

    feat: implementé un pequeño foro para consultas acerca de una propiedad. Faltaría el script para que funcione botón para enviar consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ForumComponent } from './forum/forum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideAnimationsAsync()
